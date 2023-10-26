6ab06a9806ea897f2c2f0f9553d4561aa85f778f
Sterling Watts (Mindera) <sterling.watts@2k.com>
2023-10-26 13:54:43 -0700

    create homepage with about and links

---
+++ src/components/DrawerAppBar/DrawerAppBar.tsx
@@ -0,0 +1,133 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Slide from "@mui/material/Slide";
+import roadCircle from "./Road_circle.png";
+import roadJustWordsClearBackground from "../../assets/roadJustWordsClearBackground.png";
+
+import "./DrawerAppBar.css";
+
+const drawerWidth = 240;
+const navItems = [
+  { title: "Home", href: "#home" },
+  { title: "About", href: "#about" },
+  { title: "Links", href: "#links" },
+];
+
+export default function DrawerAppBar() {
+  const isNotAtTop = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+  const scrollTrigger = useScrollTrigger({});
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box
+      className="DrawerAppBar"
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: "center" }}
+    >
+      <img
+        className="words-logo"
+        alt="road goes ever on and on"
+        src={roadJustWordsClearBackground}
+      />
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.title} disablePadding>
+            <ListItemButton href={item.href} sx={{ textAlign: "center" }}>
+              <ListItemText primary={item.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Slide appear={false} direction="down" in={!scrollTrigger}>
+        <AppBar
+          component="nav"
+          color={isNotAtTop ? "default" : "transparent"}
+          elevation={isNotAtTop ? 4 : 0}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              <img
+                className="circle-logo"
+                src={roadCircle}
+                alt="Logo"
+                height="36px"
+              />
+            </Box>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item.title}
+                  href={item.href}
+                  sx={{ color: isNotAtTop ? "#000" : "#fff" }}
+                >
+                  {item.title}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Slide>
+
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import "./index.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#587F7E",
+    },
+    secondary: {
+      main: "#8D7165",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
