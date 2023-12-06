d624202cbc7466715eb33aad4398f3b1c34d95c5
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2023-12-06 12:53:04 -0800

    refactor: 💡 Create two hooks and refactor router
    Create useUser hook to make user context easier. Create userCorp hook.
    It returns true when on a corp page. Refactor router to use modern
    implementation.

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -14,14 +14,40 @@ interface UserContext {
   signOut: () => void;
 }
 
+type Action = { type: "setUser"; payload: User } | { type: "signOut" };
+
+const reducer = (state: User | null, action: Action): User | null => {
+  switch (action.type) {
+    case "setUser":
+      return action.payload;
+    case "signOut":
+      return null;
+    default:
+      return state;
+  }
+};
+
 export const Context = React.createContext<UserContext | null>(null);
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = React.useState<User | null>(null);
+  const [user, dispatch] = React.useReducer(reducer, null);
 
-  const signOut = () => {
-    setUser(null);
-  };
+  React.useEffect(() => {
+    const user = window.sessionStorage.getItem("geouser");
+    if (user) {
+      dispatch({ type: "setUser", payload: JSON.parse(user) });
+    }
+  }, []);
+
+  const setUser = React.useCallback((user: User) => {
+    window.sessionStorage.setItem("geouser", JSON.stringify(user));
+    dispatch({ type: "setUser", payload: user });
+  }, []);
+
+  const signOut = React.useCallback(() => {
+    window.sessionStorage.removeItem("geouser");
+    dispatch({ type: "signOut" });
+  }, []);
 
   return <Context.Provider value={{ user, setUser, signOut }}>{children}</Context.Provider>;
 }
--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
+import { Navigate } from "react-router-dom";
 // components
 import Page from "../../components/Page/Page";
 import SignIn from "../../components/SignIn/SignIn";
+// hooks
+import useCorp from "../../hooks/useCorp";
 
 function SignInPage() {
+  const isCorp = useCorp();
+
   return (
     <Page className="SignInPage">
       <SignIn />
+      {!isCorp && <Navigate to="/not-found" replace />}
     </Page>
   );
 }
